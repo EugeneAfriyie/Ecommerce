7336107b5e5fe4f0dc5bbf2548450cc4b35eb23b
EugeneAfriyie <groupeight00@gmail.com>
2024-06-28 19:03:39 +0000

    Todo apdate

---
+++ code to find min and max .js
@@ -0,0 +1,56 @@
+
+  
+
+
+
+    array =[-1,2,3,4,5,6,7,8];
+    let min = 0;
+    let max = 0;
+    let value = 0;
+    for (let i = 0; i < array.length; i++){
+        
+      if (array[i] > max){
+       max = array[i]
+      }
+      else{
+        min = array[i]
+      }
+
+   
+    }
+    console.log(`min: ${min} max: ${max}`);
+    
+       
+    
+      // function minMax(nums) {
+      //   // We'll set the starting values of min and max
+      //   // as the first value in the array. (This is
+      //   // just one way to set the starting values. You
+      //   // can use another way if you like.)
+      //   const result = {
+      //     min: nums[0],
+      //     max: nums[0]
+      //   };
+
+      //   for (let i = 0; i < nums.length; i++) {
+      //     const value = nums[i];
+
+      //     // If the value is less than the min,
+      //     // update the min.
+      //     if (value < result.min) {
+      //       result.min = value;
+      //     }
+
+      //     // If the value is greater than the max,
+      //     // update the max.
+      //     if (value > result.max) {
+      //       result.max = value;
+      //     }
+      //   }
+
+      //   return result;
+      // }
+
+      // console.log(minMax([1, -3, 5]));
+      // console.log(minMax([-2, 3, -5, 7, 10]));
+

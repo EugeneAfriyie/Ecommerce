72ee54490c4b3adeddbcfe63e91741d69398d74e
EugeneAfriyie <groupeight00@gmail.com>
2024-07-10 22:39:33 +0000

    Delect product from cart when delect is click

--- PERSONAL PROJECTS/JAVASCRIPT/Amazon Projects/scripts/checkout.js
+++ PERSONAL PROJECTS/JAVASCRIPT/Amazon Projects/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './Utils/money.js'
 
@@ -37,7 +37,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delect-link" data-product-Id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -95,5 +95,12 @@ cart.forEach((cartItem) => {
   
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
+
+document.querySelectorAll('.js-delect-link').forEach((link) =>{
+  link.addEventListener('click',() =>{
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+  });
+});
 
 

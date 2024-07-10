cc6506e4f72a938550de31349720d258acc5a434
EugeneAfriyie <groupeight00@gmail.com>
2024-07-10 21:42:45 +0000

    Generated HtML for the order summary

--- PERSONAL PROJECTS/JAVASCRIPT/Amazon Projects/data/cart.js
+++ PERSONAL PROJECTS/JAVASCRIPT/Amazon Projects/data/cart.js
@@ -1,7 +1,16 @@
 export {cart}
 
 
-const cart = [];
+const cart = [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity:2
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity:1
+    }
+];
 
 
 export function addtoCart (button) {
@@ -18,6 +27,7 @@ export function addtoCart (button) {
       }else{
         cart.push({
           productId:productId,
+          quantity:1
            
           });
 

c3f73863e0433b9dd16be8708d30127ae5da652b
EugeneAfriyie <groupeight00@gmail.com>
2024-07-30 01:58:43 +0000

    portfolio update

--- PERSONAL PROJECTS/JAVASCRIPT/MY PORTFOLIO WEBSITE/porfolio.js
+++ PERSONAL PROJECTS/JAVASCRIPT/MY PORTFOLIO WEBSITE/porfolio.js
@@ -48,6 +48,12 @@ function pageTransitions(){
 pageTransitions();
 
 
+// Toggle theme 
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click' ,()=>{
+    let element = document.body;
+    element.classList.toggle('light-mode');
+})
 
 
 

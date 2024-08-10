990d78367412d0a555a9f1c9c16f8a69fdf5b3dd
EugeneAfriyie <groupeight00@gmail.com>
2024-08-10 10:20:16 +0000

    switch (e.target.value)

--- FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Training Projects/learn-modern-javascript-methods-by-building-football-team-cards/cards.js
+++ FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Training Projects/learn-modern-javascript-methods-by-building-football-team-cards/cards.js
@@ -4,11 +4,7 @@
     .map(
       ({ name, position, number, isCaptain, nickname }) =>
         `
-        <div class="player-card">
-          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
-          <p> Position: ${position}</p>
-          <p>Number: ${number}</p>
-          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+kname : "N/A"}</p>
         </div>
       `
     )

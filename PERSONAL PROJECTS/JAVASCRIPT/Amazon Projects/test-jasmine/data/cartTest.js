0d5c98a5dca7c58e980a71d110fe0b0e08f63591
EugeneAfriyie <groupeight00@gmail.com>
2024-08-15 17:44:34 +0000

    test ordersummary.js

--- PERSONAL PROJECTS/JAVASCRIPT/Amazon Projects/test-jasmine/data/cartTest.js
+++ PERSONAL PROJECTS/JAVASCRIPT/Amazon Projects/test-jasmine/data/cartTest.js
@@ -23,7 +23,6 @@ describe("Test suite:addtoCart",()=>{
         addtoCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
         expect(cart[0].quantity).toEqual(3);
         console.log(cart)
-
     })
 
 

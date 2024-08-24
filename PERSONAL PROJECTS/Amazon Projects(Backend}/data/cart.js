16cd691f85c45ee6b98b428068e785e6adaa922a
EugeneAfriyie <groupeight00@gmail.com>
2024-08-24 14:31:58 +0000

    renaming of backend amazon files

--- PERSONAL PROJECTS/Amazon Projects(Backend}/data/cart.js
+++ PERSONAL PROJECTS/Amazon Projects(Backend}/data/cart.js
@@ -10,7 +10,7 @@ export function loadFromStorage(){
 if (!cart){
   cart =[
     {
-      productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      productId:'"e43638ce-6aa0-4b85-b27f-e1d07eb678c6"',
       quantity:2,
       deliveryOptionId:'3'
     },
---
+++ PERSONAL PROJECTS/Amazon Projects(Backend}/scripts/checkout-backend.js
@@ -0,0 +1,14 @@
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+// import '../data/cart-class.js';
+import { loadProducts } from "../data/products.js";
+   
+loadProducts(() => {
+    eugene
+  });
+  
+const eugene = ()=>{
+  renderOrderSummary(); // Ensure products are loaded before rendering
+  renderPaymentSummary();
+}
+

a833e7a570d738e92a6f35faa9544beea9c6ae20
EugeneAfriyie <groupeight00@gmail.com>
2024-08-25 04:18:37 +0000

    content of selectionSort method

--- FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Training Projects/Number Sorter/Number Sorter.js
+++ FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Training Projects/Number Sorter/Number Sorter.js
@@ -36,7 +36,19 @@ const bubbleSort = (array) => {
 }
 
 const selectionSort = (array) => {
- 
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
 
   return array;
 }

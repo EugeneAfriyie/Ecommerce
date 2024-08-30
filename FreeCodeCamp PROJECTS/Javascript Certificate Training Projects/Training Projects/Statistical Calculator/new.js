105cd2b08189e0efaed59091477be7ce9789cae1
EugeneAfriyie <groupeight00@gmail.com>
2024-08-30 05:19:23 +0000

    getRange,getVariance function

--- FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Training Projects/Statistical Calculator/new.js
+++ FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Training Projects/Statistical Calculator/new.js
@@ -26,6 +26,18 @@ const getMode = (array) => {
   return mode.join(", ");
 }
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
 
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
 
 

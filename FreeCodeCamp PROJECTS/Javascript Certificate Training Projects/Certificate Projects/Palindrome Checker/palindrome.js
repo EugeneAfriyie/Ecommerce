74b4dbf1f2e289e0cb3abe8bfe32ac864f3e4724
EugeneAfriyie <groupeight00@gmail.com>
2024-09-29 13:32:41 +0000

    Palindrome Checke js

---
+++ FreeCodeCamp PROJECTS/Javascript Certificate Training Projects/Certificate Projects/Palindrome Checker/palindrome.js
@@ -0,0 +1,23 @@
+document.getElementById('check-btn').addEventListener('click', function() {
+    const textInput = document.getElementById('text-input').value;
+    const resultElement = document.getElementById('result');
+
+
+    if (textInput.trim() === '') {
+        alert('Please input a value');
+        return;
+    }
+
+   
+    function isPalindrome(str) {
+        const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, ''); 
+        return cleanStr === cleanStr.split('').reverse().join(''); 
+    }
+
+   
+    if (isPalindrome(textInput)) {
+        resultElement.textContent = `${textInput} is a palindrome`;
+    } else {
+        resultElement.textContent = `${textInput} is not a palindrome`;
+    }
+});
